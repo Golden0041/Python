4098639cd4c623ff79d5fed0ae7b198b25e5fa66
Golden0041 <GoldenStriker1101@gmail.com>
2019-05-29 20:52:57 +0400

    Coinpot miner

--- KDS/main.py
+++ KDS/main.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
 import subprocess
 
 def is_installed():
@@ -8,12 +7,19 @@ def is_installed():
 
 def install():
     # TODO: add install code
+    print("hi")
+
+def uninstall():
+    # TODO: add uninstall code
+    print("hi")
 
 def update():
     # TODO: add update code
+    subprocess.call(["git", "clone", "https://github.com/Golden0041/Python.git"])
 
 def run():
     # TODO: add run code
+    subprocess.call(["\Program Files (x86)\Google\Chrome\Application\chrome.exe", "https://coinpot.co/mine/bitcoincore/?ref=9AB12F01235C"])
 
 if is_installed():
     update()
